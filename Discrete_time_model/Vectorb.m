32d3ce2e7d1d9434baa39e0f0caf73db939386a8
bookman233 <m15755256356_1@163.com>
2024-11-21 10:47:12 +0800

    Delete Discrete_time_model directory

--- Discrete_time_model/Vectorb.m
+++
@@ -1,8 +0,0 @@
-function output=Vectorb(t,x)
-output=[exp(-i*5*t);
-        exp(-i*10*t);
-        exp(-i*5*t);
-        exp(-i*10*t);
-        exp(-i*5*t);
-        exp(-i*10*t);
-        exp(-i*10*t)+1];
